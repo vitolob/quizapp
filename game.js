42f5900edf8f549786d78e64ea2a59f236cef1cb
Victorino Machava <vitomachava@gmail.com>
2020-11-27 23:29:11 +0200

    Use Fetch API to load questions from OpenTriviaDB API

--- game.js
+++ game.js
@@ -9,33 +9,31 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+                answer : Math.floor(Math.random() * 3) + 1
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+
+            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + String(index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
+        startGame();
+    });
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
@@ -107,5 +105,3 @@ incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
 }
-
-startGame();
